d2fe462f560f1649fdc118934a5ab6fde1cc95fe
CTS\189285 <Janardhanan.SusaiManiPeter@cognizant.com>
2024-08-05 18:06:40 +0200

    Enabled swagger authorization

--- src/ncea-classifier-microservice/Program.cs
+++ src/ncea-classifier-microservice/Program.cs
@@ -26,6 +26,7 @@ using Ncea.Classifier.Microservice.Mappers;
 using Ncea.Classifier.Microservice.ExceptionHandlers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -54,8 +55,35 @@ builder.Services.AddControllers()
 });
 builder.Services.AddValidatorsFromAssemblyContaining<FilterCriteriaValidator>();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 //builder.Services.AddSwaggerGen(o => o.OperationFilter<AddRequiredHeaderParameter>());
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"        
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 var app = builder.Build();
 
