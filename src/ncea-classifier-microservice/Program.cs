2fba4d468d402ff60c6db1491701d24ecd660759
CTS\189285 <Janardhanan.SusaiManiPeter@cognizant.com>
2024-08-08 09:42:16 +0200

    sonar fixes

--- src/ncea-classifier-microservice/Program.cs
+++ src/ncea-classifier-microservice/Program.cs
@@ -70,7 +70,7 @@ builder.Services.AddSwaggerGen(option =>
                     Id="Bearer"
                 }
             },
-            new string[]{}
+            Array.Empty<string>()
         }
     });
 });
