df9e6c5a9157d2363fecb8020a83b7d4c10afb5f
Ganesh Rajamahanthy <149069705+ganesh-rajamahanthy@users.noreply.github.com>
2024-08-28 16:08:32 +0100

    Updated SearchPageContentBlocks content (#22)

---
+++ src/ncea-classifier-microservice-data/Migrations/20240823100000_ContentUpdate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Ncea.Classifier.Microservice.Data.Migrations
+{
+
+    /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
+    public partial class ContentUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE public.""SearchPageContentBlocks""
+                                    SET ""Value"" = 'Natural assets'
+                                    WHERE ""Key"" IN ('SectionTitle') AND ""Value"" = 'Natural asset';
+
+                                    UPDATE public.""SearchPageContentBlocks""
+                                    SET ""Value"" = ""Value"" || ' Select all that apply.'
+                                    WHERE ""Key"" IN ('SectionIntroduction');");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE public.""SearchPageContentBlocks""
+                                    SET ""Value"" = 'Natural asset'
+                                    WHERE ""Key"" IN ('SectionTitle') AND ""Value"" = 'Natural assets';
+
+                                    UPDATE public.""SearchPageContentBlocks""
+                                    SET ""Value"" = REPLACE(""Value"", ' Select all that apply.','')
+                                    WHERE ""Key"" IN ('SectionIntroduction');");
+        }
+    }
+}
